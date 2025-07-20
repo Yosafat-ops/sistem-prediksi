699756a0039ce3e4e520daecc0b0a9630e7c6980
Yosafat-ops <yosafatjunior00@gmail.com>
2025-07-20 10:30:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -303,4 +303,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run()
+    app.run(debug=True)
