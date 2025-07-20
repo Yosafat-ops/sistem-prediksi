469bbe9a94eabca5497298e8d0869d9e9b083f66
Yosafat-ops <yosafatjunior00@gmail.com>
2025-07-20 23:26:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import logging
 # Konfigurasi logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
